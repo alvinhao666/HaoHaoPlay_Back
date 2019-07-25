507e043ef04ff69d9fc8eab9269747f24d473c01
rongguohao <rongguohao@vip56.cn>
2019-07-25 16:37:13 +0800

    1

--- src/4-Infrastructure/Hao.Library/ErrorCode.cs
+++ src/4-Infrastructure/Hao.Library/ErrorCode.cs
@@ -5,7 +5,7 @@ namespace Hao.Library
     public static class ErrorCode
     {
 
-        public const string E100001 = "授权过期,请重新登录";
+        public const string E100001 = "授权已过期,请重新登录";
         public const string E100002 = "用户未登录,请重新登录";
         public const string E100010 = "模型验证失败";
         //基础异常
