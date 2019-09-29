c1d14041aba1435c3f439925f004683613760b45
rongguohao <rongguohao@vip56.cn>
2019-09-29 13:47:37 +0800

    1

--- src/1-WebApi/Hao.WebApi/Controllers/LoginController.cs
+++ src/1-WebApi/Hao.WebApi/Controllers/LoginController.cs
@@ -67,7 +67,7 @@ namespace Hao.WebApi
 
 
             var timeNow = DateTime.Now;
-            var validFrom = timeNow.Ticks;
+            //var validFrom = timeNow.Ticks;
 
             var claims = new Claim[]
             {
@@ -75,16 +75,16 @@ namespace Hao.WebApi
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //针对当前 token 的唯一标识
                 new Claim(JwtRegisteredClaimNames.Sid, user.ID.ToString()),
                 // new Claim(JwtRegisteredClaimNames.Typ, body.Platform.ToString()),
-                new Claim(JwtRegisteredClaimNames.Exp, timeNow.AddDays(3).Ticks.ToString(), ClaimValueTypes.Integer64),
-                new Claim(JwtRegisteredClaimNames.Iat, validFrom.ToString(), ClaimValueTypes.Integer64), //token 创建时间
-                new Claim(JwtRegisteredClaimNames.Nbf, validFrom.ToString(), ClaimValueTypes.Integer64)
+                //new Claim(JwtRegisteredClaimNames.Exp, timeNow.AddMinutes(5).Ticks.ToString(), ClaimValueTypes.Integer64),
+                //new Claim(JwtRegisteredClaimNames.Iat, validFrom.ToString(), ClaimValueTypes.Integer64), //token 创建时间
+                //new Claim(JwtRegisteredClaimNames.Nbf, validFrom.ToString(), ClaimValueTypes.Integer64)
             };
             var jwt = new JwtSecurityToken(
                 issuer: _jwtOptions.Issuer,
                 audience: _jwtOptions.Audience,
                 claims: claims,
                 notBefore:timeNow, //生效时间
-                expires: timeNow.AddDays(3),//过期时间
+                expires: timeNow.AddMinutes(1),//过期时间
                 signingCredentials: _jwtOptions.SigningKey
             );
             var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
