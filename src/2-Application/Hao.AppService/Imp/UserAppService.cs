f6a8fe32348f7bd8a0ded317187f8731e8627804
rongguohao <rongguohao@vip56.cn>
2019-10-28 09:08:26 +0800

    1

--- src/2-Application/Hao.AppService/Imp/UserAppService.cs
+++ src/2-Application/Hao.AppService/Imp/UserAppService.cs
@@ -230,7 +230,7 @@ namespace Hao.AppService
                 HFile.CreateDirectory(rootPath);
             string filePath = Path.Combine(rootPath, $"{fileName}");
 
-            await HFile.ExportToExcel(filePath, "", exportData);
+            await HFile.ExportToExcel(filePath, "用户数据", exportData);
 
             return fileName; 
         }
