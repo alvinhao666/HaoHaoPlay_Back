e6888044fe3bf9eb1ee9754fff4a86452247bcd8
rongguohao <rongguohao@vip56.cn>
2019-10-29 10:07:51 +0800

    1

--- src/5-Common/Hao.Core/UnitOfWork/UnitOfWork.cs
+++ src/5-Common/Hao.Core/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using NLog;
 using SqlSugar;
 
 namespace Hao.Core
@@ -8,6 +10,8 @@ namespace Hao.Core
     public class UnitOfWork : IUnitOfWork
     {
         public  ISqlSugarClient SqlSugarClient { get; set; }
+
+        private static ILogger _log = LogManager.GetCurrentClassLogger();
 
         // 保证每次访问，多个仓储类，都用一个 client 实例
         public ISqlSugarClient GetDbClient()
@@ -29,6 +33,7 @@ namespace Hao.Core
             catch (Exception ex)
             {
                 SqlSugarClient.Ado.RollbackTran();
+                _log.Error(ex, JsonConvert.SerializeObject(ex.Message));
             }
         }
 
