b1ba6c8968767208cb5992b5757134725e277d8c
rongguohao <rongguohao@vip56.cn>
2019-10-29 16:21:09 +0800

    1

--- src/1-WebApi/Hao.WebApi/AutoMapperInit/AutoMapperInitApi.cs
+++ src/1-WebApi/Hao.WebApi/AutoMapperInit/AutoMapperInitApi.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Hao.AppService;
+using Hao.Utility;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,7 +13,7 @@ namespace Hao.WebApi
         {
             //string.Format方法在内部使用StringBuilder进行字符串的格式化
             cfg.CreateMap<UserQueryInput, UserQuery>()
-                .ForMember(x => x.OrderFileds, a => a.MapFrom(x => x.OrderByType.HasValue && x.SortField.HasValue ? (string.Format("{0} {1}", x.SortField, x.OrderByType)) : null)); 
+                .ForMember(x => x.OrderFileds, a => a.MapFrom(x => x.SortField.CombineNameWithSpace(x.OrderByType))); 
         }
     }
 }
---
+++ src/5-Common/Hao.Utility/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Utility
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 组合排序
+        /// </summary>
+        /// <param name="sortField"></param>
+        /// <param name="orderType"></param>
+        /// <returns></returns>
+        public static string CombineNameWithSpace(this Enum sortField, Enum orderType) 
+        {
+            if (orderType != null && sortField != null )
+            {
+                return string.Format("{0} {1}", sortField, orderType);
+            }
+            return null;
+        }
+    }
+}
