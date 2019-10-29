076c7c076f375577f52b86ffaa40589d6779f1a6
rongguohao <rongguohao@vip56.cn>
2019-10-29 15:46:36 +0800

    1

--- src/1-WebApi/Hao.WebApi/AutoMapperInit/AutoMapperInitApi.cs
+++ src/1-WebApi/Hao.WebApi/AutoMapperInit/AutoMapperInitApi.cs
@@ -10,8 +10,9 @@ namespace Hao.WebApi
     {
         public static void InitMap(IMapperConfigurationExpression cfg)
         {
+            //string.Format方法在内部使用StringBuilder进行字符串的格式化
             cfg.CreateMap<UserQueryInput, UserQuery>()
-                .ForMember(x => x.OrderFileds, a => a.MapFrom(x => x.OrderByType.HasValue && x.SortField.HasValue ? (x.SortField + " " + x.OrderByType) : null));
+                .ForMember(x => x.OrderFileds, a => a.MapFrom(x => x.OrderByType.HasValue && x.SortField.HasValue ? (string.Format("{0} {1}", x.SortField, x.OrderByType)) : null)); 
         }
     }
 }
