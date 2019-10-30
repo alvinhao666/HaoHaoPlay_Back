6a4878391180825ffcbef101b7d8d7885cd82fbd
rongguohao <rongguohao@vip56.cn>
2019-10-30 16:19:58 +0800

    1

--- src/5-Common/Hao.Utility/HUtil.cs
+++ src/5-Common/Hao.Utility/HUtil.cs
@@ -25,5 +25,29 @@ namespace Hao.Utility
             TimeSpan ts = time.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalSeconds).ToString();
         }
+
+        /// <summary>
+        /// 获取指定长度的随机字符串
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private static string GetRandomStr(int length)
+        {
+            char[] arrChar = new char[]{
+           'a','b','d','c','e','f','g','h','i','j','k','l','m','n','p','r','q','s','t','u','v','w','z','y','x',
+           '0','1','2','3','4','5','6','7','8','9',
+           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Q','P','R','T','S','V','U','W','X','Y','Z'
+          };
+
+            StringBuilder num = new StringBuilder();
+
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < length; i++)
+            {
+                num.Append(arrChar[rnd.Next(0, arrChar.Length)].ToString());
+            }
+
+            return num.ToString();
+        }
     }
 }
