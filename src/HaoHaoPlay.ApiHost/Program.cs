8bc4aa29ce65990c022169232b7715e669a09cbc
rongguohao <843468011@qq.com>
2019-11-05 16:12:37 +0800

    1

--- src/HaoHaoPlay.ApiHost/Program.cs
+++ src/HaoHaoPlay.ApiHost/Program.cs
@@ -19,10 +19,6 @@ namespace HaoHaoPlay.ApiHost
 
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureContainer<ContainerBuilder>(builder =>
@@ -32,33 +28,36 @@ namespace HaoHaoPlay.ApiHost
                     builder.RegisterAssemblyTypes(
                         Assembly.Load("Hao.Repository"),
                         Assembly.Load("Hao.Core"))
-                       .Where(m => typeof(ITransientDependency).IsAssignableFrom(m) && m != typeof(ITransientDependency))
-                       .AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
+                        .Where(m => typeof(ITransientDependency).IsAssignableFrom(m) && m != typeof(ITransientDependency))
+                        .AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
 
                     builder.RegisterAssemblyTypes(
                             Assembly.Load("Hao.AppService"))
-                           .Where(m => typeof(ITransientDependency).IsAssignableFrom(m) && m != typeof(ITransientDependency))
-                           .AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired().EnableInterfaceInterceptors().InterceptedBy(typeof(HTransactionAop));
+                            .Where(m => typeof(ITransientDependency).IsAssignableFrom(m) && m != typeof(ITransientDependency))
+                            .AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired().EnableInterfaceInterceptors().InterceptedBy(typeof(HTransactionAop));
                     //一定要在你注入的服务后面加上EnableInterfaceInterceptors来开启你的拦截(aop)
 
 
                     var controllersTypesInAssembly = typeof(HController).Assembly.GetExportedTypes()
-                        .Where(type => typeof(Controller).IsAssignableFrom(type)).ToArray();
+                                    .Where(type => typeof(Controller).IsAssignableFrom(type)).ToArray();
                     builder.RegisterTypes(controllersTypesInAssembly).PropertiesAutowired();
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureLogging((hostingContext, logging) =>
-                            {
-                                logging.ClearProviders();
-                                logging.SetMinimumLevel(LogLevel.Information);
-                                logging.AddConsole();
-                                logging.AddNLog($"NLog.{hostingContext.HostingEnvironment.EnvironmentName}.config");
-                            })
-                            .UseNLog()
-                            .UseUrls("http://*:8000")
-                            .UseKestrel()
-                            .UseStartup<Startup>();
-                });
+                                                {
+                                                    logging.ClearProviders();
+                                                    logging.SetMinimumLevel(LogLevel.Information);
+                                                    logging.AddConsole();
+                                                    logging.AddNLog($"NLog.{hostingContext.HostingEnvironment.EnvironmentName}.config");
+                                                })
+                              .UseNLog()
+                              .UseUrls("http://*:8000")
+                              .UseKestrel()
+                              .UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
+        }
     }
 }
