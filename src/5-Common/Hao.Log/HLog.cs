dd74488dd3567707cebcbbb9ef75b972a29ecdfa
rongguohao <rongguohao@vip56.cn>
2019-11-08 14:16:33 +0800

    1

---
+++ src/5-Common/Hao.Log/HLog.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+
+namespace Hao.Log
+{
+    public class HLog
+    {
+        /// <summary>
+        /// 方法
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public object Argument { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
