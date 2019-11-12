f0be155c90890c15bd4ff0adeaf408a51c699e59
rongguohao <rongguohao@vip56.cn>
2019-11-12 11:10:03 +0800

    1

--- src/3-Domain/Hao.Repository/ISysUserRepository.cs
+++ src/3-Domain/Hao.Repository/ISysUserRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Hao.Core.Repository;
+﻿using Hao.Core.Repository;
 using Hao.Model;
 
 namespace Hao.Repository
