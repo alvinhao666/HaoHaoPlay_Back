0862999cd821991a836bbef2b4fe7f9b57a5c94e
rongguohao <rongguohao@vip56.cn>
2019-11-13 15:56:38 +0800

    1

---
+++ src/5-Common/Hao.Utility/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Utility
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 获取描述
+        /// </summary>
+        /// <param name="enum">枚举对象</param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum @enum)
+        {
+            if (@enum == null) return null;
+            var fieldName = @enum.ToString();
+            var enumType = @enum.GetType();
+            if (!Enum.IsDefined(enumType, @enum)) return null;
+            var hDescriptionAttribute = HDescription.Get(enumType, fieldName);
+            return hDescriptionAttribute?.Description;
+        }
+    }
+}
