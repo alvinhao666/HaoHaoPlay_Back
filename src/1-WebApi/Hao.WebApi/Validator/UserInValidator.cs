515bb48565f448d6f02572ea1e49508ff5511479
rongguohao <rongguohao@vip56.cn>
2019-11-19 15:46:21 +0800

    1

--- src/1-WebApi/Hao.WebApi/Validator/UserInValidator.cs
+++ src/1-WebApi/Hao.WebApi/Validator/UserInValidator.cs
@@ -12,6 +12,8 @@ namespace Hao.WebApi
         public UserInValidator()
         {
             RuleFor(x => x.Gender).NotEmpty().WithMessage("性别不能为空").IsInEnum().WithMessage("性别数据有误");
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("姓名不能为空");
         }
     }
 }
