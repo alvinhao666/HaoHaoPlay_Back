da8206b7aa479cc0de538af307f5876f81c297ec
rongguohao <rongguohao@vip56.cn>
2019-12-02 10:38:53 +0800

    1

--- src/5-Common/Hao.Utility/Extensions/StringExtensions.cs
+++ src/5-Common/Hao.Utility/Extensions/StringExtensions.cs
@@ -43,105 +43,15 @@ namespace Hao.Utility
         {
             return (value + "").StartsWith("System.Collections.Generic.List") || (value + "").StartsWith("System.Collections.Generic.IEnumerable");
         }
+
         public static bool IsStringArray(this string value)
         {
             return (value + "").IsMatch(@"System\.[a-z,A-Z,0-9]+?\[\]");
         }
+
         public static bool IsEnumerable(this string value)
         {
             return (value + "").StartsWith("System.Linq.Enumerable");
-        }
-
-
-        #region 转换成base64
-        public static string ToBase64String(this string s)
-        {
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(s));
-        }
-
-        public static string ToBase64String(this byte[] inArray)
-        {
-            return Convert.ToBase64String(inArray);
-        }
-
-        public static string ToBase64String(this string s, Encoding encoding)
-        {
-            return Convert.ToBase64String(encoding.GetBytes(s));
-        }
-
-        public static string ToBase64String(this byte[] inArray, Base64FormattingOptions options)
-        {
-            return Convert.ToBase64String(inArray, options);
-        }
-        #endregion
-
-        /// <summary>
-        /// 将文本字符串转换为 URL 编码的字符串。
-        /// </summary>
-        /// <param name="url">要进行 URL 编码的文本。</param>
-        /// <returns>URL 编码的字符串。</returns>
-        public static string UrlEncode (this string url)
-        {
-            return WebUtility.UrlEncode(url);
-        }
-        /// <summary>
-        /// 将已编码用于 URL 传输的字符串转换为解码的字符串。
-        /// </summary>
-        /// <param name="url">要解码的 URL 编码的字符串。</param>
-        /// <returns>返回 System.String。已解码的字符串。</returns>
-        public static string UrlDecode(this string url)
-        {
-            return WebUtility.UrlDecode(url);
-        }
-
-        public static string Join(this IEnumerable<string> values)
-        {
-            return string.Join(",", values);
-        }
-
-        public static string Join(this object[] values)
-        {
-            return string.Join(",", values);
-        }
-
-        public static string Join(this string[] value)
-        {
-            return string.Join(",", value);
-        }
-
-        public static string Join<T>(this IEnumerable<T> values)
-        {
-            return string.Join<T>(",", values);
-        }
-
-        public static string Join(this IEnumerable<string> values, string separator)
-        {
-            return string.Join(separator, values);
-        }
-
-        public static string Join(this object[] values, string separator)
-        {
-            return string.Join(separator, values);
-        }
-
-        public static string Join(this string[] value, string separator)
-        {
-            return string.Join(separator, value);
-        }
-
-        public static string Join<T>(this IEnumerable<T> values, string separator)
-        {
-            return string.Join<T>(separator, values);
-        }
-
-        public static string Join(this string[] value, int startIndex, int count)
-        {
-            return string.Join(",", value, startIndex, count);
-        }
-
-        public static string Join(this string[] value, string separator, int startIndex, int count)
-        {
-            return string.Join(separator, value, startIndex, count);
         }
     }
 }
