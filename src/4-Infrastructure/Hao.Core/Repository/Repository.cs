ebd027baf72e6bc8bc43611cfd54ea10b16a8278
rongguohao <rongguohao@vip56.cn>
2019-12-02 15:24:59 +0800

    1

--- src/4-Infrastructure/Hao.Core/Repository/Repository.cs
+++ src/4-Infrastructure/Hao.Core/Repository/Repository.cs
@@ -40,14 +40,6 @@ namespace Hao.Core
             return await UnitOfWork.GetDbClient().Queryable<T>().In(pkValues).ToListAsync();
         }
 
-        //C#主要支持 5 种动态创建对象的方式： 
-        //            1. Type.InvokeMember 
-        //            2. ContructorInfo.Invoke 
-        //            3. Activator.CreateInstance(Type) 
-        //            4. Activator.CreateInstance(assemblyName, typeName) 
-        //            5. Assembly.CreateInstance(typeName)
-        //            最快的是方式 3 ，与 Direct Create 的差异在一个数量级之内，约慢 7 倍的水平。其他方式，至少在 40 倍以上，最慢的是方式 4 ，要慢三个数量级。 
-
         /// <summary>
         /// 查询所有数据（未删除）（单表）
         /// </summary>
