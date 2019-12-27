fb83cbbd038bb0e24dadbf90876e61992babfb23
rongguohao <rongguohao@vip56.cn>
2019-12-27 10:58:04 +0800

    1

--- src/HaoHaoPlay.ApiHost/Startup.cs
+++ src/HaoHaoPlay.ApiHost/Startup.cs
@@ -124,9 +124,7 @@ namespace HaoHaoPlay.ApiHost
 
             #region Redis
 
-            var redisConnection = appsettings.ConnectionStrings.RedisConnection;
-
-            var csRedis = new CSRedisClient(redisConnection);
+            var csRedis = new CSRedisClient(appsettings.ConnectionStrings.RedisConnection);
 
             //初始化 RedisHelper
             RedisHelper.Initialization(csRedis);
@@ -138,34 +136,8 @@ namespace HaoHaoPlay.ApiHost
 
 
             #region ORM
-            var connectionConfig = new ConnectionConfig()
-            {
-                ConnectionString = appsettings.ConnectionStrings.PostgreSqlConnection,
-                DbType = DbType.PostgreSQL,
-                IsAutoCloseConnection = true,//开启自动释放模式和EF原理一样 自动释放数据务，如果存在事务，在事务结束后释放
-                InitKeyType = InitKeyType.SystemTable,  //如果不是SA等高权限数据库的账号,需要从实体读取主键或者自增列 InitKeyType要设成Attribute (不需要读取这些信息)
-                ConfigureExternalServices = new ConfigureExternalServices()
-                {
-                    DataInfoCacheService = new SqlSugarRedisCache() //RedisCache是继承ICacheService自已实现的一个类
-                }
-                //config.SlaveConnectionConfigs = new List<SlaveConnectionConfig>() { //= 如果配置了 SlaveConnectionConfigs那就是主从模式,所有的写入删除更新都走主库，查询走从库，事务内都走主库，HitRate表示权重 值越大执行的次数越高，如果想停掉哪个连接可以把HitRate设为0 
-                //     new SlaveConnectionConfig() { HitRate=10, ConnectionString=Config.ConnectionString2 },
-                //     new SlaveConnectionConfig() { HitRate=30, ConnectionString=Config.ConnectionString3 }
-            };
-#if DEBUG
-            connectionConfig.AopEvents = new AopEvents
-            {
-                OnLogExecuting = (sql, p) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine(sql);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(string.Join(",", p?.Select(it => string.Format("{0}:{1}", it.ParameterName, it.Value))));
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-            };
-#endif
-            services.AddSqlSugarClient(connectionConfig);
+
+            services.AddPostgreSQLService(appsettings.ConnectionStrings.PostgreSqlConnection);
 
             #endregion
 
