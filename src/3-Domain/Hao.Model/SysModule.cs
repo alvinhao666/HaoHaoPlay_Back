880cc079db89eff68f954a398e55f39bea0851b3
rongguohao <rongguohao@vip56.cn>
2020-03-12 19:36:18 +0800

    1

---
+++ src/3-Domain/Hao.Model/SysModule.cs
@@ -0,0 +1,33 @@
+﻿using Hao.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Model
+{
+    /// <summary>
+    /// 系统模块
+    /// </summary>
+    public class SysModule : FullAuditedEntity<long>
+    {
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 应用图标
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 路由地址
+        /// </summary>
+        public string RouterUrl { get; set; }
+
+        /// <summary>
+        /// 父级id
+        /// </summary>
+        public string ParentId { get; set; }
+    }
+}
