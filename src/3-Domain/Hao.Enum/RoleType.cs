834abc41321ac629f6a5200d374f9e8790965910
rongguohao <rongguohao@vip56.cn>
2020-03-25 17:21:03 +0800

    1

---
+++ src/3-Domain/Hao.Enum/RoleType.cs
@@ -0,0 +1,17 @@
+﻿using Hao.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Enum
+{
+    public enum RoleType
+    {
+        [HDescription("超级管理员")]
+        SuperAdministrator,
+        [HDescription("管理员")]
+        Administrator,
+        [HDescription("普通用户")]
+        User
+    }
+}
