de8696158af48d47c7100fa5d6cb8920bf2057db
rongguohao <rongguohao@vip56.cn>
2020-04-16 11:28:05 +0800

    1

--- src/1-Api/Hao.WebApi/Controllers/LoginController.cs
+++ src/1-Api/Hao.WebApi/Controllers/LoginController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Hao.AppService;
 using Hao.AppService.ViewModel;
+using Hao.Log;
 using Microsoft.AspNetCore.Mvc;
-
+using NLog;
 
 namespace Hao.WebApi
 {
@@ -12,6 +13,8 @@ namespace Hao.WebApi
     //2.行为自定义 像MVC框架的大部分组件一样，ApiControllerAttribute的行为是高度可自定义的。首先，上面说的大部分内容都是可以简单的用 on/off 来切换。具体的设置是在startup方法里面通过ApiBehaviorOptions来实现
     public class LoginController : ControllerBase
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
         private readonly ILoginAppService _loginAppService;
 
         public LoginController(ILoginAppService loginService)
@@ -25,6 +28,15 @@ namespace Hao.WebApi
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost]
-        public async Task<LoginVM> Login(LoginRequest request) => await _loginAppService.Login(request.LoginName, request.Password, request.IsRememberLogin);
+        public async Task<LoginVM> Login(LoginRequest request)
+        {
+            _logger.Info(new HLog() { Method = "Login", Argument = request, Description = "登录请求" });
+
+            var result = await _loginAppService.Login(request.LoginName, request.Password, request.IsRememberLogin);
+
+            _logger.Info(new HLog() { Method = "Login", Argument = request, Description = "登录成功" });
+
+            return result;
+        }
     }
 }
