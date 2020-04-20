e8b7569a11f0fcba5afc3b41ce2308bd303daa01
rongguohao <843468011@qq.com>
2020-04-20 22:43:28 +0800

    1

--- src/2-Application/Hao.AppService/Imp/RoleAppService.cs
+++ src/2-Application/Hao.AppService/Imp/RoleAppService.cs
@@ -78,7 +78,7 @@ namespace Hao.AppService
         public async Task UpdateRoleAuth(long id, RoleUpdateRequest vm)
         {
             var role = await GetRoleDetail(id);
-            var modules = await _moduleRep.GetListAysnc(vm.ModuleIds.Select(a=>Convert.ToInt64(a)).ToList());
+            var modules = await _moduleRep.GetListAysnc(vm.ModuleIds.Select(a=>long.Parse(a)).ToList());
             var maxLayer = modules.Max(a => a.Layer);
 
             var authNumbers = new List<long>();
