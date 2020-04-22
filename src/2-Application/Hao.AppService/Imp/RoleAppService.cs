e3c4de0ee11a3222c1bdccfdcfc0dd56a473747a
rongguohao <rongguohao@vip56.cn>
2020-04-22 16:16:02 +0800

    1

--- src/2-Application/Hao.AppService/Imp/RoleAppService.cs
+++ src/2-Application/Hao.AppService/Imp/RoleAppService.cs
@@ -172,8 +172,7 @@ namespace Hao.AppService
                     key = item.Id.ToString(),
                     title = item.Name,
                     isLeaf = item.Type == ModuleType.Resource,
-                    //expanded = (int) item.Type.Value < (int) ModuleType.Sub,
-                    expanded = true,
+                    expanded = (int)item.Type.Value < (int)ModuleType.Sub,
                     children = new List<RoleModuleItemVM>()
                 };
                 if (node.isLeaf && authNumbers?.Count > 0 && item.Layer.Value <= authNumbers.Count ) 
