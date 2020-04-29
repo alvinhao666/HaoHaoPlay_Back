6f335be070232c10c243bf6a3cd13dbfeb497f9d
rongguohao <843468011@qq.com>
2020-04-29 22:45:38 +0800

    1

--- src/1-Api/Hao.WebApi/Controllers/DictController.cs
+++ src/1-Api/Hao.WebApi/Controllers/DictController.cs
@@ -1,10 +1,20 @@
+using Hao.AppService;
 using Hao.Core.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hao.WebApi.Controllers
 {
+    /// <summary>
+    /// 数据字典
+    /// </summary>
     public class DictController:HController
     {
+        private readonly DictAppService _dictAppService;
+        public DictController(DictAppService dictAppService)
+        {
+            _dictAppService = dictAppService;
+        }
+        
         
     }
 }
