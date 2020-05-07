528b7e46feb00ab79493198213e9100e94c4dbbd
rongguohao <rongguohao@vip56.cn>
2020-05-07 14:39:11 +0800

    1

--- src/5-Common/Hao.Utility/HConvert.cs
+++ src/5-Common/Hao.Utility/HConvert.cs
@@ -118,20 +118,7 @@ namespace Hao.Utility
             else
                 return result;
         }
-      
-        /// <summary>
-        /// 将金额转换成大写人民币
-        /// </summary>
-        /// <param name="money"></param>
-        /// <returns></returns>
-        public static string ToUpperRMB(this double money)
-        {
-            if (money < 0.01)
-            {
-                throw new ArgumentOutOfRangeException("money", "转换成大写人民币失败，金额最小单位为分");
-            }
-            return Regex.Replace(Regex.Replace(money.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A"), "((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\\.]|$))))", "${b}${z}"), ".", (Match m) => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟萬億兆京垓秭穰"[(int)(m.Value[0] - '-')].ToString());
-        }
+
 
         ///// <summary>
         ///// 将指定集合按照“分隔符”拼成字符串
