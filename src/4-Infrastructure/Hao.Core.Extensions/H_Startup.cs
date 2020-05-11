bc968fa5ed198ad736315ff5927e6df386f72f90
rongguohao <rongguohao@vip56.cn>
2020-05-11 16:15:51 +0800

    1

--- src/4-Infrastructure/Hao.Core.Extensions/H_Startup.cs
+++ src/4-Infrastructure/Hao.Core.Extensions/H_Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Hao.Library;
+using Hao.RunTimeException;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,27 +17,38 @@ namespace Hao.Core.Extensions
 
         private readonly AppSettingsInfo _appSettings;
 
-        private readonly DirectoryInfo _parentDir;
-
-        protected H_Startup(IHostEnvironment env, IConfiguration cfg)
+        protected H_Startup(IHostEnvironment env, IConfiguration cfg, DirectoryInfo currentDir)
         {
-            _parentDir = new DirectoryInfo(AppContext.BaseDirectory).Parent;
-            if (_parentDir == null) throw new Exception("项目安置路径有误，请检查");
-
+            DirectoryInfo parentDir = null;
+            try
+            {
+                parentDir = currentDir?.Parent;
+                if (parentDir == null) throw new Exception("项目安置路径有误，请检查");
+            }
+            catch
+            {
+                throw new HException("项目安置路径有误，请检查");
+            }
+    
             _env = env;
             _cfg = cfg;
 
             _appSettings = new AppSettingsInfo();
             cfg.Bind("AppSettingsInfo", _appSettings);
    
-            _appSettings.FilePath.ExportExcelPath = Path.Combine(_parentDir.FullName, _appSettings.FilePath.ExportExcelPath);
-            _appSettings.FilePath.ImportExcelPath = Path.Combine(_parentDir.FullName, _appSettings.FilePath.ImportExcelPath);
-            _appSettings.FilePath.AvatarPath = Path.Combine(_parentDir.FullName, _appSettings.FilePath.AvatarPath);
+            _appSettings.FilePath.ExportExcelPath = Path.Combine(parentDir.FullName, _appSettings.FilePath.ExportExcelPath);
+            _appSettings.FilePath.ImportExcelPath = Path.Combine(parentDir.FullName, _appSettings.FilePath.ImportExcelPath);
+            _appSettings.FilePath.AvatarPath = Path.Combine(parentDir.FullName, _appSettings.FilePath.AvatarPath);
         }
 
         public virtual void ConfigureServices(IServiceCollection services)
         {
             var appSettingsOption = _cfg.GetSection(nameof(AppSettingsInfo));
+
+            appSettingsOption.GetSection(nameof(AppSettingsInfo.FilePath)).GetSection(nameof(AppSettingsInfo.FilePath.ExportExcelPath)).Value = _appSettings.FilePath.ExportExcelPath;
+            appSettingsOption.GetSection(nameof(AppSettingsInfo.FilePath)).GetSection(nameof(AppSettingsInfo.FilePath.ImportExcelPath)).Value = _appSettings.FilePath.ImportExcelPath;
+            appSettingsOption.GetSection(nameof(AppSettingsInfo.FilePath)).GetSection(nameof(AppSettingsInfo.FilePath.AvatarPath)).Value = _appSettings.FilePath.AvatarPath;
+
             services.Configure<AppSettingsInfo>(appSettingsOption);
 
             services.AddWebHost(_env, _cfg, _appSettings);
--- src/HaoHaoPlay_Back.ApiHost/Startup.cs
+++ src/HaoHaoPlay_Back.ApiHost/Startup.cs
@@ -1,12 +1,13 @@
 ﻿using Hao.Core.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace HaoHaoPlay.ApiHost
 {
     public class Startup : H_Startup
     {
-        public Startup(IHostEnvironment env, IConfiguration cfg) : base(env, cfg)
+        public Startup(IHostEnvironment env, IConfiguration cfg) : base(env, cfg, new DirectoryInfo(Directory.GetCurrentDirectory()))
         {
         }
     }
