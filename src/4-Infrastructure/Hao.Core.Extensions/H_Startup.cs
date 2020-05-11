580bda7eab798513b40189163f80b12f71e7663e
rongguohao <rongguohao@vip56.cn>
2020-05-11 14:26:23 +0800

    1

--- src/4-Infrastructure/Hao.Core.Extensions/H_Startup.cs
+++ src/4-Infrastructure/Hao.Core.Extensions/H_Startup.cs
@@ -4,9 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace Hao.Core.Extensions
 {
--- src/4-Infrastructure/Hao.Core.Extensions/ServiceCollectionExtensions.cs
+++ src/4-Infrastructure/Hao.Core.Extensions/ServiceCollectionExtensions.cs
@@ -17,7 +17,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
