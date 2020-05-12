c96e892b3afa9468d61c14fa584af006069a5853
rongguohao <rongguohao@vip56.cn>
2020-05-12 10:38:21 +0800

    1

--- src/4-Infrastructure/Hao.Library/ErrorInfo.cs
+++ src/4-Infrastructure/Hao.Library/ErrorInfo.cs
@@ -5,10 +5,7 @@
         public const string E100001 = "认证失败，请重新登录";
         public const string E100002 = "用户未登录，请重新登录";
         public const string E100003 = "用户权限值已变更，请重新登录";
-    }
 
-    public static class StringExtensions
-    {
         /// <summary>
         /// 返回错误Code
         /// </summary>
