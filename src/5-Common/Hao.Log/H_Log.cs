8a4575447cedca8fde3d75afc2bbd53c409cd1b6
rongguohao <843468011@qq.com>
2020-05-17 22:49:01 +0800

    1

---
+++ src/5-Common/Hao.Log/H_Log.cs
@@ -0,0 +1,20 @@
+namespace Hao.Log
+{
+    public class H_Log
+    {
+        /// <summary>
+        /// 方法名
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public object Argument { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
