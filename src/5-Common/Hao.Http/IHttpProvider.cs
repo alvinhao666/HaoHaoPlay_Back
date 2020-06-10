b40c987760c97a7b98327fb09cc5de651b736f23
rongguohao <rongguohao@vip56.cn>
2020-06-10 10:47:55 +0800

    1

--- src/5-Common/Hao.Http/IHttpProvider.cs
+++ src/5-Common/Hao.Http/IHttpProvider.cs
@@ -5,7 +5,7 @@ namespace Hao.Utility
 {
     public interface IHttpProvider
     {
-        Task<string> Post<TResult>(string url, Dictionary<string, string> dic, int timeoutSeconds, string contentType);
+        Task<TResult> Post<TResult>(string url, Dictionary<string, string> dic, int timeoutSeconds, string contentType);
 
         Task<TResult> Post<T, TResult>(string url, T t, int timeoutSeconds, string contentType) where T : new() where TResult : new();
     }
