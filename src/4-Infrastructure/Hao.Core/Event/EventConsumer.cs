e2d9d28217bff68d3bd8e7fc478f26aca2e456fb
rongguohao <rongguohao@vip56.cn>
2020-06-30 09:04:47 +0800

    1

--- src/4-Infrastructure/Hao.Core/Event/EventConsumer.cs
+++ src/4-Infrastructure/Hao.Core/Event/EventConsumer.cs
@@ -1,73 +1,7 @@
-﻿using AspectCore.DynamicProxy;
-using CSRedis;
-using Microsoft.Extensions.Configuration;
-using SqlSugar;
-using System;
-using System.Threading.Tasks;
-
-namespace Hao.Core
+﻿namespace Hao.Core
 {
-    public abstract class EventConsumer : IEventConsumer
+    public abstract class EventConsumer : DomainCore, IEventConsumer
     {
-        public IConfiguration Config { get; set; }
 
-        /// <summary>
-        /// 工作单元
-        /// </summary>
-        [AttributeUsage(AttributeTargets.Method)]
-        protected class UnitOfWorkAttribute : AbstractInterceptorAttribute
-        {
-            public override async Task Invoke(AspectContext context, AspectDelegate next)
-            {
-                var sqlSugarClient = context.ServiceProvider.GetService(typeof(ISqlSugarClient)) as ISqlSugarClient;
-                try
-                {
-#if DEBUG
-                    Console.ForegroundColor = ConsoleColor.Blue;
-
-                    Console.WriteLine("开始事务：" + sqlSugarClient.ContextID);
-#endif
-                    sqlSugarClient.Ado.BeginTran();
-                    await next(context);
-#if DEBUG
-                    Console.ForegroundColor = ConsoleColor.Blue;
-
-                    Console.WriteLine("提交事务：" + sqlSugarClient.ContextID);
-#endif
-                    sqlSugarClient.Ado.CommitTran();
-                }
-                catch (Exception ex)
-                {
-#if DEBUG
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("回滚事务：" + sqlSugarClient.ContextID);
-#endif
-                    sqlSugarClient.Ado.RollbackTran();
-                    throw ex; //抛给上层接收
-                }
-            }
-        }
-
-        /// <summary>
-        /// 开启分布式锁
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="timeoutSeconds"></param>
-        /// <param name="autoDelay"></param>
-        /// <returns></returns>
-        protected CSRedisClientLock Lock(string name, int timeoutSeconds = 10, bool autoDelay = true)
-        {
-            var prefix = Config["RedisPrefix:Lock"];
-
-            if (string.IsNullOrWhiteSpace(prefix)) throw new Exception("分布式锁前缀字符不能为空");
-
-            var redisLock = RedisHelper.Lock($"{prefix}{name}", timeoutSeconds, autoDelay);
-
-            if (redisLock == null) throw new Exception("开启分布式锁异常");
-
-            //if (redisLock == null) throw new H_Exception("系统异常"); //开启分布式锁超时 //对象为null，不占资源 ，编译后的代码没有fianlly,不执行dispose()方法
-            return redisLock;
-        }
     }
 }
