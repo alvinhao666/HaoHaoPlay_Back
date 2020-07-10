725a2e60cdb6b4fd041455d6ee6159ea6391a647
rongguohao <rongguohao@vip56.cn>
2020-07-10 15:48:21 +0800

    1

--- src/4-Infrastructure/Hao.Core.Extensions/UtilExtensions.cs
+++ src/4-Infrastructure/Hao.Core.Extensions/UtilExtensions.cs
@@ -22,60 +22,13 @@ namespace Hao.Core.Extensions
             {
                 ip = httpContext.Connection.RemoteIpAddress.ToString();
 
-                if (ip.Contains("::ffff"))
-                {
-                    ip = ip.Split("::ffff:")[1].Trim(); //::ffff:192.168.2.144
-                }
-                else if (ip == "::1" || ip.Contains("127.0.0.1"))
+                if (ip.Contains("::ffff") || ip == "::1" || ip.Contains("127.0.0.1"))
                 {
                     ip = "127.0.0.1";
                 }
             }
 
             return ip;
-        }
-
-        /// <summary>
-        /// 获取局域网IP
-        /// </summary>
-        private static string GetLanIp()
-        {
-            foreach (var hostAddress in Dns.GetHostAddresses(Dns.GetHostName()))
-            {
-                if (hostAddress.AddressFamily == AddressFamily.InterNetwork)
-                    return hostAddress.ToString();
-            }
-            return string.Empty;
-        }
-
-        /// <summary>
-        /// 获取局域网IP
-        /// </summary>
-        /// <param name="type">网络接口类型</param>
-        private static string GetLanIp(NetworkInterfaceType type)
-        {
-            try
-            {
-                foreach (var item in NetworkInterface.GetAllNetworkInterfaces())
-                {
-                    if (item.NetworkInterfaceType != type || item.OperationalStatus != OperationalStatus.Up)
-                        continue;
-                    var ipProperties = item.GetIPProperties();
-                    if (ipProperties.GatewayAddresses.FirstOrDefault() == null)
-                        continue;
-                    foreach (var ip in ipProperties.UnicastAddresses)
-                    {
-                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
-                            return ip.Address.ToString();
-                    }
-                }
-            }
-            catch
-            {
-                return string.Empty;
-            }
-
-            return string.Empty;
         }
     }
 }
