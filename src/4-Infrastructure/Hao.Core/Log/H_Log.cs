4264fb2e76efb875e4b309249a3842bb88eeda28
rongguohao <rongguohao@vip56.cn>
2020-07-29 08:39:30 +0800

    1

---
+++ src/4-Infrastructure/Hao.Core/Log/H_Log.cs
@@ -0,0 +1,38 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace Hao.Log
+{
+    public class H_Log
+    {
+        /// <summary>
+        /// 方法名
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public object Argument { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 重写方法，必须，用于日志记录
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions()
+            {
+                //在默认情况下，System.Text.Json 序列化程序对所有非 ASCII 字符进行转义；这就是中文被转义的根本原因
+                //可以不必设置例外而达到不转义的效果，这个模式就是“非严格JSON”模式
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNamingPolicy = null
+            });
+        }
+    }
+}
