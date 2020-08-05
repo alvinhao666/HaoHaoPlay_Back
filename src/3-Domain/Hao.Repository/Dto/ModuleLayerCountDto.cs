4210eec60605691953280116d17d618860f71fca
rongguohao <rongguohao@vip56.cn>
2020-08-05 13:09:46 +0800

    1

---
+++ src/3-Domain/Hao.Repository/Dto/ModuleLayerCountDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Model
+{
+    //数据传输对象（DTO)(Data Transfer Object)，是一种设计模式之间传输数据的软件应用系统。
+    //数据传输目标往往是 数据访问对象从数据库中 检索数据。数据传输对象 与数据交互对象或数据访问对象之间的差异是一个以不具有任何行为   除了存储和检索的数据（访问和存取器）。
+    public class ModuleLayerCountDto
+    {
+        /// <summary>
+        /// 层
+        /// </summary>
+        public int? Layer { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int? Count { get; set; }
+    }
+}
