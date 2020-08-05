262ca9c946a546c8e4da0d66e68a1be699bab4f7
rongguohao <rongguohao@vip56.cn>
2020-08-05 13:13:07 +0800

    1

--- src/3-Domain/Hao.Repository/ISysModuleRepository.cs
+++ src/3-Domain/Hao.Repository/ISysModuleRepository.cs
@@ -7,7 +7,7 @@ namespace Hao.Repository
     public interface ISysModuleRepository : IRepository<SysModule, long>
     {
         /// <summary>
-        /// 获取每一层的数量，包括已删除的，最多64个
+        /// 获取每一层的数量，包括已删除的，最多31个 0~30
         /// </summary>
         /// <returns></returns>
         Task<ModuleLayerCountDto> GetLayerCount();
