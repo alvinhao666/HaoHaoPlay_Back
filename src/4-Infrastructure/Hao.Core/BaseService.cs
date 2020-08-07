791adc7ffa9fedc57ae49f4d56a3a6d3e02da734
rongguohao <rongguohao@vip56.cn>
2020-08-07 11:09:00 +0800

    1

--- src/4-Infrastructure/Hao.Core/BaseService.cs
+++ src/4-Infrastructure/Hao.Core/BaseService.cs
@@ -29,6 +29,9 @@ namespace Hao.Core
             if (redisLock == null) throw new Exception("开启分布式锁异常");
 
             //if (redisLock == null) throw new H_Exception("系统异常"); //开启分布式锁超时 //对象为null，不占资源 ，编译后的代码没有fianlly,不执行dispose()方法
+            //锁超时是什么意思呢？如果一个得到锁的线程在执行任务的过程中挂掉，来不及显式地释放锁，这块资源将会永远被锁住，别的线程再也别想进来。
+
+            //所以，setnx的key必须设置一个超时时间，以保证即使没有被显式释放，这把锁也要在一定时间后自动释放。
             return redisLock;
         }
 
