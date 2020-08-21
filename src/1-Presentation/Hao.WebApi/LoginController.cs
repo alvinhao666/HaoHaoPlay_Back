d23959cf0cb9a0a6933ce676b0a58450241d18ca
rongguohao <rongguohao@vip56.cn>
2020-08-21 09:24:09 +0800

    1

--- src/1-Presentation/Hao.WebApi/LoginController.cs
+++ src/1-Presentation/Hao.WebApi/LoginController.cs
@@ -17,13 +17,13 @@ namespace Hao.WebApi.Controllers
     //2.行为自定义 像MVC框架的大部分组件一样，ApiControllerAttribute的行为是高度可自定义的。首先，上面说的大部分内容都是可以简单的用 on/off 来切换。具体的设置是在startup方法里面通过ApiBehaviorOptions来实现
     public class LoginController : ControllerBase
     {
-        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger(); //顺序1
 
         private readonly ILoginAppService _loginAppService;
 
         public LoginController(ILoginAppService loginService)
         {
-            _loginAppService = loginService;
+            _loginAppService = loginService; //顺序2
         }
 
         /// <summary>
