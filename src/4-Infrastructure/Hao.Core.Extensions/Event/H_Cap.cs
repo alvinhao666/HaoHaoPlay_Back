ac6bb76de3cecd73135af8e63752297c46de090b
rongguohao <rongguohao@vip56.cn>
2020-09-02 09:53:55 +0800

    1

--- src/4-Infrastructure/Hao.Core.Extensions/Event/H_Cap.cs
+++ src/4-Infrastructure/Hao.Core.Extensions/Event/H_Cap.cs
@@ -26,6 +26,13 @@ namespace Microsoft.Extensions.DependencyInjection
                     cfg.Password = config.Password;
                 });
 
+                //订阅者组的概念类似于 Kafka 中的消费者组，它和消息队列中的广播模式相同，用来处理不同微服务实例之间同时消费相同的消息。
+
+                //当CAP启动的时候，她将创建一个默认的消费者组，如果多个相同消费者组的消费者消费同一个Topic消息的时候，只会有一个消费者被执行。
+                 
+                //相反，如果消费者都位于不同的消费者组，则所有的消费者都会被执行。
+
+                //相同的实例中，你可以通过下面的方式来指定他们位于不同的消费者组。
                 x.DefaultGroup = config.DefaultGroup;
 
                 x.FailedRetryCount = 10; //失败重试机会，默认50次
