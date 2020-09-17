ba6973ab7950bc948f2ad0405e4882c18aff419b
rongguohao <rongguohao@vip56.cn>
2020-09-17 14:04:15 +0800

    1

--- src/4-Infrastructure/Hao.Core/QueryInput/QueryInput.cs
+++ src/4-Infrastructure/Hao.Core/QueryInput/QueryInput.cs
@@ -1,12 +1,16 @@
-﻿namespace Hao.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hao.Core
 {
     /// <summary>
     /// 输入的查询条件
     /// </summary>
     public class QueryInput : IPagedQuery
     {
+        [Range(1, int.MaxValue, ErrorMessage = "PageIndex必须大于0")]
         public int PageIndex { get; set; } = 1;
 
+        [Range(1, 100, ErrorMessage = "PageSize不能超过100")] //防止恶意大量数据查询而导致数据库瘫痪
         public int PageSize { get; set; } = 10;
 
         public H_OrderByType? OrderByType { get; set; }
