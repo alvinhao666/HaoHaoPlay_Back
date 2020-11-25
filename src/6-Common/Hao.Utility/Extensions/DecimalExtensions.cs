b091747668e07a39883c028d820d58418c431c3d
rongguohao <rongguohao@vip56.cn>
2020-11-25 14:45:44 +0800

    1

---
+++ src/6-Common/Hao.Utility/Extensions/DecimalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hao.Utility
+{
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// 金额保留两位小数
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ToMoneyString(this decimal? value)
+        {
+            if (!value.HasValue) return "";
+
+            return value.Value.ToString("#0.##");
+        }
+    }
+}
