482c7a01baeae12f9fe83f99630582e2ceebda6b
rongguohao <rongguohao@vip56.cn>
2020-12-10 10:32:42 +0800

    1

--- src/6-Common/Hao.Utility/Extensions/DecimalExtensions.cs
+++ src/6-Common/Hao.Utility/Extensions/DecimalExtensions.cs
@@ -1,21 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Hao.Utility
 {
     public static class DecimalExtensions
     {
         /// <summary>
-        /// 金额保留两位小数
+        /// 去除小数点后面的0
         /// </summary>
-        /// <param name="time"></param>
+        /// <param name="obj"></param>
         /// <returns></returns>
-        public static string ToMoneyString(this decimal? value)
+        public static string ClearZeroDigit(this decimal? obj)
         {
-            if (!value.HasValue) return "";
+            if (!obj.HasValue) return "0";
 
-            return value.Value.ToString("#0.##");
+            return obj.Value.ToString("#0.##########");
         }
+
+        /// <summary>
+        /// 保留小数位数，默认2位，四舍五入  
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="decimals"></param>
+        /// <returns></returns>
+        public static decimal KeepSmallDigit(this decimal? obj, int decimals = 2)
+        {
+            //0.995 ---- 1
+            //0.994 ---- 0.99
+            if (!obj.HasValue) return 0;
+            return decimal.Round(obj.Value, decimals, MidpointRounding.AwayFromZero);
+        }
+
     }
 }
