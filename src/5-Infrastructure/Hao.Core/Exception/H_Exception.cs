0fd77de8b7d136e3948dbc4e4806948eed55e28e
rongguohao <rongguohao@vip56.cn>
2020-12-29 10:38:48 +0800

    增加异常判断 断言

--- src/5-Infrastructure/Hao.Core/Exception/H_Exception.cs
+++ src/5-Infrastructure/Hao.Core/Exception/H_Exception.cs
@@ -22,4 +22,44 @@ namespace Hao.Core
 
         public H_Exception(string message, Exception innerException) : base(message, innerException) { }
     }
+
+
+    public static class H_Assert
+    {
+        public static void IsTrue<T>(bool condition, string msg) where T : Exception, new()
+        {
+            if (condition)
+            {
+                var ex = Activator.CreateInstance(typeof(T), new object[] { msg }) as T;
+                throw ex;
+            }
+        }
+
+        public static void IsTrue<T>(bool condition, string msg, int code) where T : Exception, new()
+        {
+            if (condition)
+            {
+                var ex = Activator.CreateInstance(typeof(T), new object[] { msg, code }) as T;
+                throw ex;
+            }
+        }
+
+        public static void IsFalse<T>(bool condition, string msg) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                var ex = Activator.CreateInstance(typeof(T), new object[] { msg }) as T;
+                throw ex;
+            }
+        }
+
+        public static void IsFalse<T>(bool condition, string msg, int code) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                var ex = Activator.CreateInstance(typeof(T), new object[] { msg, code }) as T;
+                throw ex;
+            }
+        }
+    }
 }
