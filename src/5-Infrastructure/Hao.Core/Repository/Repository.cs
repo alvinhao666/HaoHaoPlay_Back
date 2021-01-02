7f2123b028b4b339f3a83f6c20c6c47d0da8d412
rongguohao <843468011@qq.com>
2021-01-02 20:04:23 +0800

    仓储修改

--- src/5-Infrastructure/Hao.Core/Repository/Repository.cs
+++ src/5-Infrastructure/Hao.Core/Repository/Repository.cs
@@ -12,24 +12,11 @@ namespace Hao.Core
     public abstract class Repository<T, TKey> : IRepository<T, TKey>
         where T : Entity<TKey>, new() where TKey : struct
     {
-        [FromServiceContext] protected ICurrentUser CurrentUser { get; set; }
-
         [FromServiceContext] protected IdWorker IdWorker { get; set; }
         
-        private IFreeSqlContext _dbContext;
+        [FromServiceContext] protected ICurrentUser CurrentUser { get; set; }
         
-        [FromServiceContext]
-        protected IFreeSqlContext DbContext
-        {
-            get {  return _dbContext;  }
-            
-            set
-            {
-                // 根据当前用户信息  不同租户 可以设置不同的查询条件 全局过滤器  例如以下 
-                // value.GlobalFilter.ApplyOnly<ICompanyInfo>(nameof(ICompanyInfo),x => x.CompanyId == CurrentUser.CompanyId); // 相同名称的过滤器 最后的会覆盖前面的
-                _dbContext = value;
-            }
-        }
+        [FromServiceContext] protected IFreeSqlContext DbContext { get; set; }
 
         /// <summary>
         /// 根据主键查询单条数据
--- src/5-Infrastructure/Hao.Core/Repository/SimpleRepository.cs
+++ src/5-Infrastructure/Hao.Core/Repository/SimpleRepository.cs
@@ -12,10 +12,11 @@ namespace Hao.Core
     public abstract class SimpleRepository<T, TKey> : ISimpleRepository<T, TKey>
         where T : SimpleEntity<TKey>, new() where TKey : struct
     {
+        [FromServiceContext] protected IdWorker IdWorker { get; set; }
+        
         [FromServiceContext] protected IFreeSqlContext DbContext { get; set; }
 
-        [FromServiceContext] protected IdWorker IdWorker { get; set; }
-
+        
         /// <summary>
         /// 根据主键值查询单条数据
         /// </summary>s
