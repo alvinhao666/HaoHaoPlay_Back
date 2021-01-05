96c825a530ff686e491a88ee06bda3f86203aa94
rongguohao <rongguohao@vip56.cn>
2021-01-05 14:58:33 +0800

    删除废弃代码

--- src/6-Common/Hao.Dependency/DependencyServiceCollectionExtensions.cs
+++ src/6-Common/Hao.Dependency/DependencyServiceCollectionExtensions.cs
@@ -10,29 +10,6 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <summary>
         /// 自动IOC扫描注入
         /// </summary>
-        public static IServiceCollection AutoDependency(this IServiceCollection services, IEnumerable<Type> types)
-        {
-            services.Scan(scan => scan.FromAssembliesOf(types)
-                    .AddClasses(x => x.AssignableTo(typeof(ITransientDependency))) //直接或间接实现了ITransientDependency
-                    .AsImplementedInterfaces()
-                    .WithTransientLifetime())
-                
-                .Scan(scan => scan.FromAssembliesOf(types)
-                    .AddClasses(x => x.AssignableTo(typeof(IScopeDependency)))
-                    .AsImplementedInterfaces()
-                    .WithScopedLifetime())
-                
-                .Scan(scan => scan.FromAssembliesOf(types)
-                    .AddClasses(x => x.AssignableTo(typeof(ISingletonDependency)))
-                    .AsImplementedInterfaces()
-                    .WithSingletonLifetime());
-
-            return services;
-        }
-
-        /// <summary>
-        /// 自动IOC扫描注入
-        /// </summary>
         public static IServiceCollection AutoDependency(this IServiceCollection services,
             IEnumerable<Assembly> assemblies)
         {
