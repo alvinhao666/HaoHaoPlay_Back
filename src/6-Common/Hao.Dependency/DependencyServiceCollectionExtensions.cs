cf08e18bf9e03c896b8d6b0c441b3b5f1f2c9a5f
rongguohao <rongguohao@vip56.cn>
2021-01-20 17:02:46 +0800

    1

--- src/6-Common/Hao.Dependency/DependencyServiceCollectionExtensions.cs
+++ src/6-Common/Hao.Dependency/DependencyServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class DependencyServiceCollectionExtensions
     {
         /// <summary>
-        /// 自动IOC扫描注入
+        /// 扫描注入到IOC容器中
         /// </summary>
         public static IServiceCollection AutoDependency(this IServiceCollection services,
             IEnumerable<Assembly> assemblies)
@@ -27,6 +27,29 @@ namespace Microsoft.Extensions.DependencyInjection
                     .AddClasses(x => x.AssignableTo(typeof(ISingletonDependency)))
                     .AsImplementedInterfaces()
                     .WithSingletonLifetime());
+            return services;
+        }
+
+        /// <summary>
+        /// 扫描注入到IOC容器中
+        /// </summary>
+        public static IServiceCollection AutoDependency(this IServiceCollection services, IEnumerable<Type> types)
+        {
+            services.Scan(scan => scan.FromAssembliesOf(types)
+                    .AddClasses(x => x.AssignableTo(typeof(ITransientDependency))) //直接或间接实现了ITransientDependency
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime())
+
+                .Scan(scan => scan.FromAssembliesOf(types)
+                    .AddClasses(x => x.AssignableTo(typeof(IScopeDependency)))
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime())
+
+                .Scan(scan => scan.FromAssembliesOf(types)
+                    .AddClasses(x => x.AssignableTo(typeof(ISingletonDependency)))
+                    .AsImplementedInterfaces()
+                    .WithSingletonLifetime());
+
             return services;
         }
     }
