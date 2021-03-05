c365ea91b22937f816c280f2da870e2257b452e6
rongguohao <rongguohao@vip56.cn>
2021-03-05 16:05:00 +0800

    1

--- src/5-Infrastructure/Hao.Core.Extensions/Middleware/ExceptionMiddleware.cs
+++ src/5-Infrastructure/Hao.Core.Extensions/Middleware/ExceptionMiddleware.cs
@@ -62,8 +62,8 @@ namespace Hao.Core.Extensions
             }
 #endif
             if (string.IsNullOrWhiteSpace(response.ErrorMsg)) response.ErrorMsg = "系统异常";
-            
-            Log.Error(ex, "系统错误信息 TraceId:{TraceId}，Path:{Path}", context.TraceIdentifier, context.Request.Path.Value); //异常信息，记录到日志中
+
+            Log.Error(ex, "系统错误信息:{@Log}", new H_Log() { Position = context.Request.Path.Value, TraceId = context.TraceIdentifier }); //异常信息，记录到日志中
 
             var options = new JsonSerializerOptions
             {
