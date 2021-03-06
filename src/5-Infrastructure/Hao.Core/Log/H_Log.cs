30b485bb7ecf9ee9dd004c14831417bd3e44ce66
rongguohao <843468011@qq.com>
2021-03-06 18:19:26 +0800

    日志修改

--- src/5-Infrastructure/Hao.Core/Log/H_Log.cs
+++ src/5-Infrastructure/Hao.Core/Log/H_Log.cs
@@ -6,7 +6,7 @@ namespace Hao.Core
     /// <summary>
     /// 日志
     /// </summary>
-    public static  class H_Log
+    public static class H_Log
     {
         /// <summary>
         /// 默认模板
@@ -18,44 +18,44 @@ namespace Hao.Core
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Verbose(string content) => Log.Verbose(content);
-        
+
         /// <summary>
         /// Verbose
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Verbose(LogConent content) => Log.Verbose(_defaultTemplate, content);
-        
+
         /// <summary>
         /// Debug
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Debug(string content) => Log.Debug(content);
-        
+
         /// <summary>
         /// Debug
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Debug(LogConent content) => Log.Debug(_defaultTemplate, content);
-        
+
         /// <summary>
         /// Info
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Info(string content) => Log.Information(content);
-        
+
         /// <summary>
         /// Info
         /// </summary>
         /// <param name="conent">日志内容</param>
         public static void Info(LogConent conent) => Log.Information(_defaultTemplate, conent);
-        
-        
+
+
         /// <summary>
         /// Warn
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Warn(string content) => Log.Warning(content);
-        
+
         /// <summary>
         /// Warn
         /// </summary>
@@ -67,20 +67,20 @@ namespace Hao.Core
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Error(string content) => Log.Error(content);
-        
+
         /// <summary>
         /// Error
         /// </summary>
         /// <param name="ex">异常</param>
         /// <param name="conent">日志内容</param>
         public static void Error(Exception ex, LogConent conent) => Log.Error(ex, _defaultTemplate, conent);
-        
+
         /// <summary>
         /// Fatal
         /// </summary>
         /// <param name="content">日志内容</param>
         public static void Fatal(string content) => Log.Fatal(content);
-        
+
         /// <summary>
         /// Fatal
         /// </summary>
