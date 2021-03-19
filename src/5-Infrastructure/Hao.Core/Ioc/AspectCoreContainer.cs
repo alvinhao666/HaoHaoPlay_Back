ce109a04987f7c48c2556f847210ddc6c869227a
rongguohao <rongguohao@vip56.cn>
2021-03-19 11:11:05 +0800

    aspectcorecontainer修改

--- src/5-Infrastructure/Hao.Core/Ioc/AspectCoreContainer.cs
+++ src/5-Infrastructure/Hao.Core/Ioc/AspectCoreContainer.cs
@@ -1,58 +1,58 @@
-﻿using AspectCore.Configuration;
-using AspectCore.DependencyInjection;
-using AspectCore.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using AspectCore.Configuration;
+//using AspectCore.DependencyInjection;
+//using AspectCore.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace Hao.Core
-{
-    public class AspectCoreContainer
-    {
-        private static IServiceResolver resolver { get; set; }
+//namespace Hao.Core
+//{
+//    public class AspectCoreContainer
+//    {
+//        private static IServiceResolver  _resolver;
 
-        public static IServiceProvider Build(IServiceCollection services)
-        {
-            if (services == null) throw new ArgumentNullException(nameof(services));
+//        public static IServiceProvider Build(IServiceCollection services)
+//        {
+//            if (services == null) throw new ArgumentNullException(nameof(services));
 
-            return resolver = services.ToServiceContext().Build();
-        }
+//            return _resolver = services.ToServiceContext().Build();
+//        }
 
-        public static T Resolve<T>()
-        {
-            if (resolver == null)
-                throw new ArgumentNullException(nameof(resolver), "调用此方法时必须先调用BuildServiceProvider！");
-            return resolver.Resolve<T>();
-        }
+//        public static T Resolve<T>()
+//        {
+//            if (_resolver == null)
+//                throw new ArgumentNullException(nameof(_resolver), "调用此方法时必须先调用BuildServiceProvider！");
+//            return _resolver.Resolve<T>();
+//        }
 
-        public static object Resolve(Type type)
-        {
-            if (resolver == null)
-                throw new ArgumentNullException(nameof(resolver), "调用此方法时必须先调用BuildServiceProvider！");
-            return resolver.Resolve(type);
-        }
+//        public static object Resolve(Type type)
+//        {
+//            if (_resolver == null)
+//                throw new ArgumentNullException(nameof(_resolver), "调用此方法时必须先调用BuildServiceProvider！");
+//            return _resolver.Resolve(type);
+//        }
 
-        public static object Resolve(string typeName)
-        {
-            if (resolver == null)
-                throw new ArgumentNullException(nameof(resolver), "调用此方法时必须先调用BuildServiceProvider！");
-            return resolver.Resolve(Type.GetType(typeName));
-        }
+//        public static object Resolve(string typeName)
+//        {
+//            if (_resolver == null)
+//                throw new ArgumentNullException(nameof(_resolver), "调用此方法时必须先调用BuildServiceProvider！");
+//            return _resolver.Resolve(Type.GetType(typeName));
+//        }
 
-        public static List<T> ResolveServices<T>()
-        {
-            if (resolver == null)
-                throw new ArgumentNullException(nameof(resolver), "调用此方法时必须先调用BuildServiceProvider！");
-            return resolver.GetServices<T>().ToList();
-        }
+//        public static List<T> ResolveServices<T>()
+//        {
+//            if (_resolver == null)
+//                throw new ArgumentNullException(nameof(_resolver), "调用此方法时必须先调用BuildServiceProvider！");
+//            return _resolver.GetServices<T>().ToList();
+//        }
 
-        public static List<T> ResolveServices<T>(Func<T, bool> filter)
-        {
-            if (resolver == null)
-                throw new ArgumentNullException(nameof(resolver), "调用此方法时必须先调用BuildServiceProvider！");
-            if (filter == null) filter = m => true;
-            return ResolveServices<T>().Where(filter).ToList();
-        }
-    }
-}
+//        public static List<T> ResolveServices<T>(Func<T, bool> filter)
+//        {
+//            if (_resolver == null)
+//                throw new ArgumentNullException(nameof(_resolver), "调用此方法时必须先调用BuildServiceProvider！");
+//            if (filter == null) filter = m => true;
+//            return ResolveServices<T>().Where(filter).ToList();
+//        }
+//    }
+//}
